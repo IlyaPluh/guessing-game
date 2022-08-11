c772888bcc95576def1501419085919340bbd8e3
IlyaPluh <ilyaandriyanov@mail.ru>
2022-08-11 13:23:43 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
     constructor() {}
-
+    #min;
+    #max;
+    #candidate;
+  
     setRange(min, max) {
-
+      this.#min = min;
+      this.#max = max;
     }
-
+  
     guess() {
-
+      return this.#candidate = Math.ceil((this.#min + this.#max) / 2);
     }
-
+  
     lower() {
-
+      this.#max = this.#candidate;
     }
-
+  
     greater() {
-
+      this.#min = this.#candidate;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
